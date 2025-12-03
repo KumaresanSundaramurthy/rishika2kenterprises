fed1f636e47f9277083e621a89e487dca78c0fd9
KumaresanSundaramurthy <kumarbtechguru@gmail.com>
2025-12-03 11:39:29 +0530

    Redis Implemented

--- application/config/redis.php
+++ application/config/redis.php
@@ -1,10 +1,10 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 $config['redis'] = [
-    'host'     => 'r2kportal-redis',
-    'port'     => 6379,
-    'password' => NULL,
-    'database' => 0,
+    'host'     => getenv('PHP_REDIS_HOST'),
+    'port'     => getenv('PHP_REDIS_PORT'),
+    'password' => getenv('PHP_REDIS_PASSWORD'),
+    'database' => getenv('PHP_REDIS_DB'),
     'timeout'  => 1.0,   // connection timeout seconds
     'persistent' => FALSE
 ];
